99ac50afd3d9501fdc31e2b85ad325d0f17ae2be
jisookim <jisookimwork@gmail.com>
2024-05-13 17:45:23 +0900

    플레이어 움직이기

--- main.cpp
+++ main.cpp
@@ -4,18 +4,67 @@ using namespace std;
 
 int main()
 {
-	int	Number1 = 10;
-	int Number2 = 20;
+	int Map[10][10]
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
 
-	cin >> Number1;
-	cin >> Number2;
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char Key = 0;
 
-	cout << Number1 + Number2 << endl;
-	cout << Number1 - Number2 << endl;
-	cout << Number1 / Number2 << endl;
-	cout << Number1 * Number2 << endl;
 
-	return 0;
+	for (;;)
+	{
+		cin >> Key; //wasd
+		if (Key == 'w')
+		{
+			PlayerY--;
+		}
+		else if (Key == 's')
+		{
+			PlayerY++;
+		}
+		else if (Key == 'a')
+		{
+			PlayerX--;
+		}
+		else if (Key == 'd')
+		{
+			PlayerX++;
+		}
+
+		system("cls");
+		for (int Y = 0; Y < 10; ++Y)
+		{
+			for (int X = 0; X < 10; ++X)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "p";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << " ";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					cout << "*";
+				}
+			}
+			cout << endl;
+		}
+	}
+		return 0;
 
 }
 
