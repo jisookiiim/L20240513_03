12a76867588d3d2b9cd6e37828223e17a50178d8
jisookim <jisookimwork@gmail.com>
2024-05-13 10:10:41 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>	
+
+using namespace std;
+
+int main()
+{
+	int	Number1 = 10;
+	int Number2 = 20;
+
+	cin >> Number1;
+	cin >> Number2;
+
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	cout << Number1 * Number2 << endl;
+
+	return 0;
+
+}
+
